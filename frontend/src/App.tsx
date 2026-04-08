bbb583fd2c2b101b3a829760ad322318f15493d6
Jale <jalencess@gmail.com>
2026-04-08 14:34:27 +0500

    feat: add clearing cart after payment successful

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useSearchParams } from 'react-router-dom'
+import { useEffect } from 'react'
 import Header from './widgets/Header'
 import Home from './pages/Home'
 import Inventory from './pages/Inventory'
 import styles from './App.module.css'
 import Callback from './pages/Callback'
+import { useCart } from './shared/hooks/useCart'
+
+function PurchaseHandler() {
+  const [params, setParams] = useSearchParams()
+  const { clear } = useCart()
+
+  useEffect(() => {
+    if (params.get('status') === 'done') {
+      clear()
+      setParams({}, { replace: true })
+    }
+  }, [])
+
+  return null
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className={styles.app}>
+        <PurchaseHandler />
         <Header />
         <main className={styles.main}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/inventory" element={<Inventory />} />
-            <Route path="/battlepass" element={<div>Battle Pass</div>} />
-            <Route path="/cart" element={<div>Cart</div>} />
             <Route path="/callback" element={<Callback />} />
           </Routes>
         </main>
